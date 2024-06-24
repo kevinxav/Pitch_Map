52d50274684293f169e2fa724177bae5f4f0378c
Kevin Xaviour <116493448+kevinxav@users.noreply.github.com>
2024-06-24 10:59:00 +0530

    Update pitch_map.py

--- pitch_map.py
+++ pitch_map.py
@@ -229,11 +229,14 @@ def filter_and_plot(data, batsman, run_types, zip_file, output_dir):
     if not data.empty:
         batting_type = data['StrikerBattingType'].iloc[0]
 
-        if batting_type == 1:
-            image_path = 'Pacer_R.png'
-        elif batting_type == 2:
-            image_path = 'pitchL.jpg'
-            
+        if batting_type == 1 and PaceorSpin == 2:
+            image_path = 'Spinner_R.png'
+        elif batting_type == 2 and PaceorSpin == 2:
+            image_path = 'Spinner_R.png'
+        elif batting_type == 1:
+            image_path == 'PitchR.jpg'
+        else:
+            image_path == 'PitchL.jpg'
         img = Image.open(image_path)
         img_array = plt.imread(image_path)
         height, width, _ = img_array.shape
