f392be94a7a64803fdd40e8dbcdd1c99e7c95cd3
Kevin Xaviour <116493448+kevinxav@users.noreply.github.com>
2024-06-24 11:32:59 +0530

    Update pitch_map.py

--- pitch_map.py
+++ pitch_map.py
@@ -198,7 +198,7 @@ def main():
                 filtered_data_batsman = filtered_data[filtered_data['StrikerName'] == batsman]
                 
                 if not filtered_data_batsman.empty:
-                    filter_and_plot(filtered_data_batsman, batsman, run_types, zip_file, output_dir,PaceorSpin)
+                    filter_and_plot(filtered_data_batsman, batsman, run_types, zip_file, output_dir,pace_or_spin)
         
         st.download_button(
             label="Download ZIP",
